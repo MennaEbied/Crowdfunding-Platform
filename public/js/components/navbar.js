855b51209a9ca2f7443b06ff7e6ed904c73402ee
MennaEbied <mennahesham253@gmail.com>
2026-03-10 23:34:11 +0200

    Update navbar to handle guest mode

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -1,15 +1,29 @@
 function Navbar() {
   const navRight = document.querySelector(".nav-right");
+  const navLeft = document.querySelector(".nav-left");
   const user = JSON.parse(localStorage.getItem("currentUser"));
 
   if (!user) {
+    //GUEST MODE
     navRight.innerHTML = `
             <a href="/pages/login.html">Login</a>
             <a href="/pages/register.html">Register</a>
         `;
+    navLeft.innerHTML = `
+        <a href="/index.html">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact Us</a>
+        `;
   } else {
+    //USER MODE
+    navLeft.innerHTML = `
+      <a href="/index.html">Home</a>
+        <a href="/pages/campaign.html">Launch Campaign</a>
+        <a href="#">About</a>
+        <a href="/pages/profile.html">My Pledges</a>
+      `;
     let links = `<span class="user-welcome">Hello, ${user.name}</span>`;
-    links += `<a href="/pages/profile.html">My Pledges</a>`;
+    links += ``;
     if (user.role === "admin") {
       links += `<a href="/pages/admin.html">Admin Dashboard</a>`;
     }
