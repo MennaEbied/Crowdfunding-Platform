19d44060a13cd9daef705a4c4217a8b961a99cd4
MennaEbied <mennahesham253@gmail.com>
2026-03-23 20:52:01 +0200

    Update navbar structure

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -6,34 +6,51 @@ function Navbar() {
   if (!user) {
     //GUEST MODE
     navRight.innerHTML = `
-            <a href="/pages/login.html">Login</a>
-            <a href="/pages/register.html">Register</a>
+            <button id="login-btn">Login</button>
+            <button id="register-btn">Register</button>
         `;
     navLeft.innerHTML = `
         <a href="/index.html">Home</a>
         <a href="#">About</a>
         <a href="#">Contact Us</a>
         `;
+
+    document.getElementById("login-btn").addEventListener("click", () => {
+      window.location.href = "/pages/login.html";
+    });
+    document.getElementById("register-btn").addEventListener("click", () => {
+      window.location.href = "/pages/register.html";
+    });
   } else {
     //USER MODE
     navLeft.innerHTML = `
       <a href="/index.html">Home</a>
         <a href="/pages/campaign.html">Launch Campaign</a>
-        <a href="#">About</a>
-        <a href="/pages/profile.html">My Pledges</a>
+        ${user.role === "admin" ? '<a href="/pages/admin.html">Dashboard</a>' : ""}
+        <a href="/pages/profile.html">Profile</a>
       `;
-    let links = `<span class="user-welcome">Hello, ${user.name}</span>`;
-    links += ``;
-    if (user.role === "admin") {
-      links += `<a href="/pages/admin.html">Admin Dashboard</a>`;
-    }
-
-    links += `<button id="logout-btn">Logout</button>`;
-    navRight.innerHTML = links;
+    let rightLinks = `<span class="user-welcome">Hello, ${user.name}</span>`;
+    rightLinks += `<button id="logout-btn">Logout</button>`;
+    navRight.innerHTML = rightLinks;
 
     document.getElementById("logout-btn").addEventListener("click", () => {
       localStorage.removeItem("currentUser");
-      window.location.href = "/index.html";
+       sessionStorage.removeItem("currentUser");
+      window.location.replace("/index.html");
+    });
+  }
+  const mobileNav = document.querySelector(".mobile-nav");
+  const navMenu = document.querySelector(".nav-menu");
+  if (mobileNav) {
+    mobileNav.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+
+      const icon = mobileNav.querySelector("i");
+      if (navMenu.classList.contains("active")) {
+        icon.classList.replace("fa-bars", "fa-xmark");
+      } else {
+        icon.classList.replace("fa-xmark", "fa-bars");
+      }
     });
   }
 }
